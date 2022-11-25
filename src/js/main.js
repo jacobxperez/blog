9db8bc39a4934e9ca3b7af7eba77b65392b51ad7
Jacob <jacobxperez@gmx.com>
2022-11-25 15:08:37 -0700

    Update color palette

--- src/js/main.js
+++ src/js/main.js
@@ -140,7 +140,7 @@ document.addEventListener("DOMContentLoaded", () => {
         )
         .getTemplate("asideTemplate", "content")
         .getTemplate("contentTemplate", "content")
-        .fetchSource("/main.html", "navTemplate", "header")
-        .fetchSource("/main.html", "footerTemplate", "footerContent");
+        .fetchSource("/templates/index.html", "navTemplate", "header")
+        .fetchSource("/templates/index.html", "footerTemplate", "footerContent");
     // always leave footer at the end for toggles to work
 });
