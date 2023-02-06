f4f860d767789baeb234721a39f6b2244654441f
Jacob <jacobxperez@gmx.com>
2023-02-05 22:50:54 -0700

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -140,7 +140,7 @@ document.addEventListener("DOMContentLoaded", () => {
         )
         .getTemplate("asideTemplate", "content")
         .getTemplate("contentTemplate", "content")
-        .fetchSource("/templates/index.260bfb36.html", "navTemplate", "header")
-        .fetchSource("/templates/index.260bfb36.html", "footerTemplate", "footerContent");
+        .fetchSource("/templates/index.c3e3e0fd.html", "navTemplate", "header")
+        .fetchSource("/templates/index.c3e3e0fd.html", "footerTemplate", "footerContent");
     // always leave footer at the end for toggles to work
 });
