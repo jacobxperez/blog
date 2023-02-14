99941000ef1912e3820588f8f9426792b05aa02e
Jacob <jacobxperez@gmx.com>
2023-02-13 22:29:44 -0700

    Added prettier

--- src/js/main.js
+++ src/js/main.js
@@ -4,88 +4,82 @@
  * http://www.apache.org/licenses/LICENSE-2.0
 -----------------------------------------------------------------------------*/
 // Check if site is on local host
-location.hostname === "localhost" || location.hostname === "127.0.0.1"
-    ? (baseURL = window.location.origin)
-    : (baseURL = window.location.origin + "/blog");
+location.hostname === "localhost" || location.hostname === "127.0.0.1" ? (baseURL = window.location.origin) : (baseURL = window.location.origin + "/blog")
 
 // Document Data
 var docData = {
     title: document.title,
     subTitle: null,
     // author: document.querySelector('[name=author]').content,
-};
+}
 
 // toggles
 function toggle() {
-    const getToggle = document.querySelectorAll("[data-toggle]");
-    const getToolTip = document.querySelectorAll("[data-tooltip]");
+    const getToggle = document.querySelectorAll("[data-toggle]")
+    const getToolTip = document.querySelectorAll("[data-tooltip]")
 
     // toggles attribute
     function toggleAttr(item) {
         item.addEventListener("click", (e) => {
-            !item.hasAttribute("data-state", "active")
-                ? item.setAttribute("data-state", "active")
-                : item.removeAttribute("data-state");
-            e.stopPropagation();
-        });
+            !item.hasAttribute("data-state", "active") ? item.setAttribute("data-state", "active") : item.removeAttribute("data-state")
+            e.stopPropagation()
+        })
     }
 
-    getToggle.forEach(toggleAttr);
-    getToolTip.forEach(toggleAttr);
+    getToggle.forEach(toggleAttr)
+    getToolTip.forEach(toggleAttr)
 
     function removeAtt(item, match, e) {
         if (e.target !== item && item.matches(match)) {
-            item.removeAttribute("data-state");
+            item.removeAttribute("data-state")
         }
     }
 
     document.addEventListener("click", (e) => {
-        getToggle.forEach((item) => removeAtt(item, '[data-toggle="pop"]', e));
-        getToolTip.forEach((item) => removeAtt(item, "[data-tooltip]", e));
-    });
+        getToggle.forEach((item) => removeAtt(item, '[data-toggle="pop"]', e))
+        getToolTip.forEach((item) => removeAtt(item, "[data-tooltip]", e))
+    })
 }
 
 const template = {
     parser: new DOMParser(),
     _copyPasteTemplate(templateID, targetID, _source) {
         // get template ID from source
-        const _getTemplateID = _source.getElementById(templateID);
+        const _getTemplateID = _source.getElementById(templateID)
         // clone template ID from source
-        const _cloneTemplate = _getTemplateID.content.cloneNode(true);
+        const _cloneTemplate = _getTemplateID.content.cloneNode(true)
         // get target ID from page
-        const _targetID = document.getElementById(targetID);
+        const _targetID = document.getElementById(targetID)
         // append template to target ID
-        _targetID.appendChild(_cloneTemplate);
+        _targetID.appendChild(_cloneTemplate)
         // delete original template from document
-        _getTemplateID.remove();
+        _getTemplateID.remove()
     },
     _parseSource(source, templateID, targetID, mimeType) {
-        if (mimeType === undefined) mimeType = "text/html";
+        if (mimeType === undefined) mimeType = "text/html"
         // get source and parse it
-        const _source = this.parser.parseFromString(source, mimeType);
-        this._copyPasteTemplate(templateID, targetID, _source);
+        const _source = this.parser.parseFromString(source, mimeType)
+        this._copyPasteTemplate(templateID, targetID, _source)
     },
     getTemplate(templateID, targetID, _source = document) {
         new Promise((resolve, reject) => {
             // check if template exist if not reject
-            !templateID ? reject() : resolve();
+            !templateID ? reject() : resolve()
         })
             .then(() => this._copyPasteTemplate(templateID, targetID, _source))
-            .catch((err) => console.error(err));
+            .catch((err) => console.error(err))
 
-        return this;
+        return this
     },
     fromString(source, templateID, targetID, mimeType) {
         new Promise((resolve, reject) => {
             // check if source is string
-            typeof source === "string"
-                ? resolve()
-                : reject((err = "Error: Source is not a String"));
+            typeof source === "string" ? resolve() : reject((err = "Error: Source is not a String"))
         })
             .then(() => this._parseSource(source, templateID, targetID, mimeType))
-            .catch((err) => console.error(err));
+            .catch((err) => console.error(err))
 
-        return this;
+        return this
     },
     fetchSource(source, templateID, targetID, mimeType) {
         fetch(baseURL + source)
@@ -95,18 +89,16 @@ const template = {
             .catch((err) => console.error(err))
             .finally(() => {
                 // once the footer is loaded start toggles
-                if (templateID === "footerTemplate") toggle();
-            });
+                if (templateID === "footerTemplate") toggle()
+            })
 
-        return this;
+        return this
     },
-};
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     // Check for subtitle then added to layout
-    !docData.subTitle
-        ? (subTitle = "")
-        : (subTitle = `<h2 data-text="h5">${docData.subTitle}</h2>`);
+    !docData.subTitle ? (subTitle = "") : (subTitle = `<h2 data-text="h5">${docData.subTitle}</h2>`)
 
     // 1. generate page layout from string
     // 2. move secondary content to layout
@@ -141,6 +133,6 @@ document.addEventListener("DOMContentLoaded", () => {
         .getTemplate("asideTemplate", "content")
         .getTemplate("contentTemplate", "content")
         .fetchSource("/templates/index.3df2d2ca.html", "navTemplate", "header")
-        .fetchSource("/templates/index.3df2d2ca.html", "footerTemplate", "footerContent");
+        .fetchSource("/templates/index.3df2d2ca.html", "footerTemplate", "footerContent")
     // always leave footer at the end for toggles to work
-});
+})
