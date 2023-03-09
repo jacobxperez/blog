523fa7967e9225f8ab46235628fcfdefe90acf0e
Jacob <jacobxperez@gmx.com>
2023-03-09 00:34:58 -0700

    Update initial.js
    Added comments

--- src/js/initial.js
+++ src/js/initial.js
@@ -1,3 +1,9 @@
+/* @license
+ * <https://github.com/jacobxperez/blog/>
+ * Copyright (C) 2023 Jacob Perez <jacobxperez@gmx.com>
+ * Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+------------------------------------------------------------------------------*/
 const docData = {
     title: document.title,
     subtitle: '',
