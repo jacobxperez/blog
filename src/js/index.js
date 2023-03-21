82f291e69eaef9529b07c97e81d6cb15934f0cab
Jacob <jacobxperez@gmx.com>
2023-03-20 23:41:21 -0600

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 /* @license
- * Jacob Perez <https://jacobxperez.github.io/vannelli/>
+ * Jacob Perez <https://jacobxperez.github.io/blog/>
  * Copyright (C) 2022 Jacob Perez <jacobxperez@gmx.com>
  * Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
