7748409e5efa6162a275ba7a56146b24bab3938d
Jacob <jacobxperez@gmx.com>
2023-03-21 13:48:22 -0600

    Rename file template.js to layout.js
    move layout.js to folder modules

--- src/js/modules/layout.js
+++ src/js/modules/layout.js
@@ -4,8 +4,8 @@
  * Licensed under the Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
 -----------------------------------------------------------------------------*/
-import {sidebar} from './modules/sidebar.js';
-import {toggle} from './modules/toggle';
+import {sidebar} from './sidebar.js';
+import {toggle} from './toggle';
 
 if (template.title === '') {
     template.title = `<h1>Jacob Perez</h1>`;
